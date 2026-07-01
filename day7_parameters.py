7bfcb77692fdd84e24cc7363be1c2a8497591981
Edwin Meka <edwinchukwuemeka12@gmail.com>
2026-07-01 10:34:20 +0100

    Day 7: Learned parameters, arguments and return

---
+++ day7_parameters.py
@@ -0,0 +1,17 @@
+#def introduce(name, age):
+    #print("My name is", name)
+    #print("I am", age, "years old")
+    #print()
+
+#introduce("Edwin", 25)
+#introduce("Alice", 30)
+#introduce("Bob", 22)
+
+def student(name, course):
+    print("student:", name)
+    print("course:", course)
+    print()
+
+student("Edwin", "Python")
+student("Alice", "Java")
+student("Bob", "Ai Automation")
