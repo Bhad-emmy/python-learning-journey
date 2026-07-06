ad00d3fe8c5c7296d3893aed25a529317b43040d
Edwin Meka <edwinchukwuemeka12@gmail.com>
2026-07-06 08:16:25 +0100

    Day 9: Learned Python tuples

---
+++ day9_tuples.py
@@ -0,0 +1,23 @@
+cars = ("Toyota", "Honda", "BMW")
+
+print(cars[2])
+
+
+
+
+animals = (
+    "Dog",
+    "Cat",
+    "Lion",
+    "Tiger",
+    "Goat"
+)
+
+print(len(animals))
+
+
+
+numbers = (2, 4, 6)
+
+for number in numbers:
+    print(number * 3)
